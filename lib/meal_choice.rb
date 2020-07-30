ddf51ebc97790deb6638c6cf1f1a4ae088faeac3
Rahel Hagos <hagos.rahelh@gmail.com>
2020-07-30 07:10:07 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,6 +1,7 @@
 # Your code here
 # For output purposes, use "puts" instead of "print" or "p"
 
+<<<<<<< HEAD
 def meal_choice (veg1, veg2, protein = 'tofu') 
   puts"What a nutritious meal!"
   
@@ -15,3 +16,10 @@ puts meal_choice('broccoli', 'macaroni')
 
 
 
+=======
+def meal_choice 
+  
+  
+  
+end
+>>>>>>> 4ebf12a1d8328168b90e3cf530a79d002c63eacf
